64fdfa780aeaa1a5f391a6da5d6b675d17ea7236
Gladys <gladys.hzamaya@gmail.com>
2026-07-29 17:12:19 -0500

    feat(config): register ScreeningProperties

--- src/main/java/com/bulwark/BulwarkApplication.java
+++ src/main/java/com/bulwark/BulwarkApplication.java
@@ -1,12 +1,13 @@
 package com.bulwark;
 
 import com.bulwark.config.UpstreamProperties;
+import com.bulwark.screening.ScreeningProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(UpstreamProperties.class)
+@EnableConfigurationProperties({UpstreamProperties.class, ScreeningProperties.class})
 public class BulwarkApplication {
 
     public static void main(String[] args) {
